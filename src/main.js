e0e109faa89a8b6ecd135fceb875e59cca90abf2
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-07-04 11:12:23 -0500

    Agregando pseudo-codigo para codear

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+//Aquí se debe manipular el DOM
+
+//Declarar una variable en donde se guarde la llamada de la función con el acceso a la data
+//Declarar todos los botones que usaremos
+
+//Crear nodo
+//Pasarle los datos
+
+console.log(POKEMON);
 console.log(POKEMON.pokemon[0].name);
