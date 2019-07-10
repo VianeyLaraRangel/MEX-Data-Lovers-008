4ce555d9ba87147a2a545f11e41d69bec230e8f6
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-07-10 17:20:09 -0500

    Prueba para pull request a la rama de desarrollo

--- src/data.js
+++ src/data.js
@@ -1,20 +1,24 @@
-//Hacer una llave y hacer de su propiedad una función
-//Dentro de la función, crear una variable que sea un arreglo vacío
-//Hacer un ciclo,bucle o loop(for in o for each)
-//Cuando recorramos la data, crear dentro del blucle las variables que acceden a la info
-//Usar un método para introducir la data al arreglo (la data está en objetos)
-//Retornar el nuevo arreglo
-//Invocar la función dándole como argumento el arreglo
-
-//Esta es una función de ejemplo, puedes ver como agregamos la función a nuestro objeto global window
 const getData = () => {
-  const pokemonesArray = POKEMON.pokemon;
-  console.log(pokemonesArray);
-  
-  
+  const pokemonesArray = [];
+  const data = POKEMON.pokemon;
+
+  pokemonesArray.push(data);
+  //console.log(pokemonesArray);
   return 'getData';
 };
 
+//filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+
+// sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+//computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
+
+
 //Agregarlo al objeto global
 window.getData = getData();
+
+//1.Mostrar la data en una interfaz: puede ser un card, una tabla, una lista, etc.
+//2.Permitir al usuario filtrar y ordenar la data.
+//3.Calcular estadísticas de la colección (o subcolección) como media aritmética, máximo y/o mínimo de algún atributo numérico, o contar cuántas veces aparece un determinado valor, por ejemplo.
+//4.Visualizarse sin problemas desde distintos tamaños de pantallas: móviles, tablets y desktops.
 
