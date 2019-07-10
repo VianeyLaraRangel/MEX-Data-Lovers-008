1132c0630b438e7566085d1e7ac3d214f6f1bee2
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-07-10 17:33:38 -0500

    Creando las funciones puras sin contenido para tener claros los objetivos

--- src/data.js
+++ src/data.js
@@ -1,3 +1,4 @@
+//Objetivo: 1.Mostrar la data en una interfaz: puede ser un card, una tabla, una lista, etc.
 const getData = () => {
   const pokemonesArray = [];
   const data = POKEMON.pokemon;
@@ -7,18 +8,27 @@ const getData = () => {
   return 'getData';
 };
 
-//filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+//Objetivo: 2.Permitir al usuario filtrar y ordenar la data. Como?filterData(data, condition): esta función filter o filtrar recibiría la data, y nos retornaría aquellos datos que sí cumplan con la condición.
+const filterData = (data, condition) => {
 
-// sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+  return 'filterData';
+};
+
+//Objetivo: 3.Calcular estadísticas de la colección (o subcolección) como media aritmética, máximo y/o mínimo de algún atributo numérico, o contar cuántas veces aparece un determinado valor, por ejemplo. Como? sortData(data, sortBy, sortOrder): esta función sort u ordenar recibe tres parámetros. El primer parámetro, data, nos entrega los datos. El segundo parámetro, sortBy, nos dice con respecto a cuál de los campos de la data se quiere ordenar. El tercer parámetro, sortOrder, indica si se quiere ordenar de manera ascendente o descendente.
+
+const sortData = (data, sortBy, sortOrder) => {
+
+  return 'sortData';
+};
 
 //computeStats(data): la función compute o calcular, nos permitirá hacer cálculos estadísticos básicos para ser mostrados de acuerdo a la data proporcionada.
+const computeStats = (data) => {
 
+  return 'computeStats';
+};
 
 //Agregarlo al objeto global
 window.getData = getData();
-
-//1.Mostrar la data en una interfaz: puede ser un card, una tabla, una lista, etc.
-//2.Permitir al usuario filtrar y ordenar la data.
-//3.Calcular estadísticas de la colección (o subcolección) como media aritmética, máximo y/o mínimo de algún atributo numérico, o contar cuántas veces aparece un determinado valor, por ejemplo.
-//4.Visualizarse sin problemas desde distintos tamaños de pantallas: móviles, tablets y desktops.
-
+window.filterData = filterData();
+window.sortData = sortData();
+window.computeStats = computeStats();
