91632c5cfc5415516794379208d75868aad73291
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-07-13 22:07:00 -0500

    Añadiendo modal de instrucciones

--- src/main.js
+++ src/main.js
@@ -1,9 +1,43 @@
-//onload = window.alert('Bienvenido a pokepedia!');
 //Variables globales con el acceso a la data para iterar
 const pokemonesArray = POKEMON.pokemon;
 const pokemonesObject = POKEMON.pokemon[0];
 const cardTemplate = document.getElementById('card-box-specific');
 
+//Botones
+const buttonHomePokemones = document.getElementById('pokebola-imprime-todos');
+//Boton instrucciones
+const buttonHelp = document.getElementById('ayuda-modal');
+const closeButtonHelp = document.getElementById('close');
+
+//Función que esconde una sección con id
+const hideSection = (id) => {
+    document.getElementById(id).classList.add('hide');
+};
+//funcion que muestra una seccion con id
+const showSection = (id) => {
+    document.getElementById(id).classList.remove('hide');
+};
+
+//Funcionalidad del modal
+const showInstructions = () => showSection('instructions');
+const closeInstructions = () => hideSection('instructions');
+
+buttonHelp.addEventListener('click', showInstructions);
+closeButtonHelp.addEventListener('click', closeInstructions);
+
+
+const buttonSearchPokemon = document.getElementById('busca-pokemon');
+const buttonAsc = document.getElementById('boton-asc');
+const buttonDesc = document.getElementById('boton-desc');
+//Seis (6) botones
+const buttonPokeType = document.getElementById('poison-type');
+
+//Nodos
+const cardBox = document.getElementById('card-box');
+const cardsGeneral = document.getElementById('cards-general');
+const cardResult = document.getElementById('imprime-nodo');
+
+//Funciones declaradas
 const printData = (pokemonesArray) => {
     let str = '';
 
@@ -21,32 +55,31 @@ const printData = (pokemonesArray) => {
             <span>Debilidades:</span><br>
           </p>
         </div>
-      </div>`
+      </div>`;
     });
     cardTemplate.innerHTML = str;
 };
+//Eventos
 
-
-
-//Declarar todos los botones que usaremos
-const buttonHomePokemones = document.getElementById('pokebola-imprime-todos');
-const buttonHelp = document.getElementById('ayuda-modal');
-const buttonSearchPokemon = document.getElementById('busca-pokemon');
-const buttonAsc = document.getElementById('boton-asc');
-const buttonDesc = document.getElementById('boton-desc');
-//Seis (6) botones
-const buttonPokeType = document.getElementById('poison-type');
-
-//Declarar variables en donde se imprimen los nodos
-const cardBox = document.getElementById('card-box');
-const cardsGeneral = document.getElementById('cards-general');
-const cardResult = document.getElementById('imprime-nodo');
-
-//a)Evento para imprimir todos los pokemones en la pantalla principal
 buttonHomePokemones.addEventListener('click', () => {
     printData(pokemonesArray);
 });
-buttonHelp.addEventListener('click', () => {
+
+
+buttonSearchPokemon.addEventListener('click', () => {
+    alert('funciona');
+});
+
+buttonAsc.addEventListener('click', () => {
+    alert('funciona');
+});
+
+buttonDesc.addEventListener('click', () => {
+    alert('funciona');
+});
+
+//Primer boton de los tipos para ordenar
+buttonPokeType.addEventListener('click', () => {
     //Escribir un bucle que itere por el arreglo
     for (let i = 0; i < pokemonesArray.length; i++) {
         let pokemonesObject = POKEMON.pokemon[i];
@@ -81,21 +114,4 @@ buttonHelp.addEventListener('click', () => {
 
         cardBox.appendChild(cardResult);
     }
-});
-
-buttonSearchPokemon.addEventListener('click', () => {
-    alert('funciona');
-});
-
-buttonAsc.addEventListener('click', () => {
-    alert('funciona');
-});
-
-buttonDesc.addEventListener('click', () => {
-    alert('funciona');
-});
-
-//Primer boton de los tipos para ordenar
-buttonPokeType.addEventListener('click', () => {
-    alert('funciona');
 });
