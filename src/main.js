7fdf1780390897a3a3d95606476e8e24a8f0eb3b
Vianey Lara Rangel <vianey.lara.rangel@gmail.com>
2019-07-13 19:23:16 -0500

    Vuelve a funcionar el nodo

--- src/main.js
+++ src/main.js
@@ -1,75 +1,78 @@
+//onload = window.alert('Bienvenido a pokepedia!');
 //Variables globales con el acceso a la data para iterar
-//Puedo ver el arreglo con 151 objetos (C/U pokemones)
-//console.log(pokeData);
-//Variables en donde se imprimen los nodos
-const cardBox = document.getElementById('card-box');
-console.log(cardBox);
-const cardResult = document.getElementById('imprime-nodo');
-console.log(cardResult);
+const pokemonesArray = POKEMON.pokemon;
+const pokemonesObject = POKEMON.pokemon[0];
+
 //Declarar todos los botones que usaremos
 const buttonHomePokemones = document.getElementById('pokebola-imprime-todos');
 const buttonHelp = document.getElementById('ayuda-modal');
 const buttonSearchPokemon = document.getElementById('busca-pokemon');
 const buttonAsc = document.getElementById('boton-asc');
 const buttonDesc = document.getElementById('boton-desc');
+//Seis (6) botones
 const buttonPokeType = document.getElementById('poison-type');
 
+//Declarar variables en donde se imprimen los nodos
+const cardBox = document.getElementById('card-box');
+const cardsGeneral = document.getElementById('cards-general');
+const cardResult = document.getElementById('imprime-nodo');
 
-//Eventos
 //a)Evento para imprimir todos los pokemones en la pantalla principal
 buttonHomePokemones.addEventListener('click', () => {
-    //Escribir un bucle que itere por el arreglo
-    for (let i = 0; i < window.getData.length; i++) {
-        let pokemonesObject = POKEMON.pokemon[i];
-        console.log(pokemonesObject);
-        //Crear elementos (html)
-        let pokeName = document.createElement('h2');
-        let pokeImg = document.createElement('img');
-        let pokeType = document.createElement('p');
-        //Hacer nodos de los valores
-        let pokeNameContent = document.createTextNode(pokemonesObject.name);
+   //Escribir un bucle que itere por el arreglo
+   for (let i = 0; i < pokemonesArray.length; i++) {
+           let pokemonesObject = POKEMON.pokemon[i];
+           let printCard = Object.keys(pokemonesObject);
 
-        let pokeImgContent = document.createTextNode(pokemonesObject.img);
+   //Crear elementos (html)
+           let pokeName = document.createElement('h2');
+           let pokeImg = document.createElement('img');
+           let pokeType = document.createElement('p');
+   //Crear nodos (contenido de los hijos para su madre/padre)
+       let pokeNameContent = document.createTextNode(pokemonesObject.name);
+       console.log(pokeNameContent);
 
-        let pokeTypeContent = document.createTextNode(Object.values(pokemonesObject.type));
+       let pokeImgContent = document.createTextNode(pokemonesObject.img);
+       console.log(pokeImgContent);
 
-        //Hacer de los contenidos, hijxs de los elementos
-        pokeName.appendChild(pokeNameContent);
-        pokeName.setAttribute('align', 'center');
+       let pokeTypeContent = document.createTextNode(Object.values(pokemonesObject.type));
+       console.log(pokeTypeContent);
+  
+   //Hacer de los nodos hijos de los elementos
+       pokeName.appendChild(pokeNameContent);
+       pokeName.setAttribute('align', 'center');
 
-        pokeImg.appendChild(pokeImgContent);
-        pokeImg.setAttribute('src', pokemonesObject.img);
+       pokeImg.appendChild(pokeImgContent);
+       pokeImg.setAttribute('src', pokemonesObject.img);
+      
+       pokeType.appendChild(pokeTypeContent);
+  
+      
+       cardResult.appendChild(pokeName);
+       cardResult.appendChild(pokeImg);
+       cardResult.appendChild(pokeType);
 
-        pokeType.appendChild(pokeTypeContent);
-
-        cardResult.appendChild(pokeName);
-        cardResult.appendChild(pokeImg);
-        cardResult.appendChild(pokeType);
-
-        cardBox.appendChild(cardResult);
-    }
+       cardBox.appendChild(cardResult);
+     }
 });
 
 buttonHelp.addEventListener('click', () => {
-    alert('funciona');
+   alert('funciona');
 });
 
 buttonSearchPokemon.addEventListener('click', () => {
-    //alert('funcionaaa');
-
+   alert('funciona');
 });
 
 buttonAsc.addEventListener('click', () => {
-    alert('funciona');
+   alert('funciona');
 });
 
 buttonDesc.addEventListener('click', () => {
-    alert('funciona');
+   alert('funciona');
 });
 
 //Primer boton de los tipos para ordenar
 buttonPokeType.addEventListener('click', () => {
-    
+   alert('funciona');
 });
-
-//Invocaciones
